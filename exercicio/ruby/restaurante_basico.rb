816385dce6c728f8c25257903e04552e32a5aec2
Rodrigo <rodrigorcarv@gmail.com>
2016-08-03 23:27:33 -0300

    # Implementacao de estrura de controle e regex - Teste de estrutura de controle: if

--- exercicio/ruby/restaurante_basico.rb
+++ exercicio/ruby/restaurante_basico.rb
@@ -2,3 +2,18 @@
 nome = gets
 
 print "Nome do restaurante: #{nome}" 
+
+
+#estrutura de controle: if
+puts "Digite a nota do restaurante"
+
+#estrutura gets to convert para Fixnum
+nota = gets.to_i
+
+
+if (nota > 7)
+	puts "Boa nota!"
+else
+	puts "Nota ruim"
+end
+
